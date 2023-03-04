01a5a594976a350a7d885ed85b0fe919c1084148
Oliiiv <weng.zhang0708@gmail.com>
2023-03-03 22:37:19 -0500

    SV2 database set & frontend setup with auth0

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import mongoose from 'mongoose';
+import cors from 'cors'
+import expressSession from 'express-session';
+import { usersRouter } from './routes/users.js';
+
+const PORT = 5000;
+const connection = "mongodb+srv://wengzhang0708:SHETOUweng0708@cluster0.m4hzbmi.mongodb.net/?retryWrites=true&w=majority";
+const sessionName = "SkillvitrineUser";
+const app = express();
+
+
+try{
+    mongoose.connect(connection, { ssl: true }).catch(error => console.log(error));
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log("Could not connect to MongoDB");
+}
+
+app.use(helmet());
+app.use(cors({ origin: 'http://localhost:5000', credentials: true }));
+app.use(expressSession({
+    name: sessionName,
+    secret: 'Skillvitrine',
+    resave: false,
+    saveUninitialized: false,
+}));
+app.set('trust proxy', 1);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+
+app.listen(PORT, (err) => {
+    if (err) console.log(err);
+    else console.log("HTTP server on http://localhost:%s", PORT);
+  });
+
---
+++ backend/routes/users.js
@@ -0,0 +1,134 @@
+import express from 'express';
+import auth0 from 'auth0-js';
+import bcrypt from "bcrypt";
+import { Router } from "express";
+import { User } from '../model/userModel.js';
+
+
+const url = "http://localhost:3000";
+const domain = "dev-a6l74pvzmwqg5h8s.us.auth0.com";
+const clientID = "OX8IzCO9NRge4U7OT3jTl4T32Wx4a9qu";
+const callbackUrl = "http://localhost:3000/users/callback";
+
+export const usersRouter = Router();
+
+usersRouter.post('/signup', (req, res) => {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+
+  if(!req.body.username){
+    return res.status(400).json({ error: "Username is required." });
+  }
+  if(!req.body.email){
+    return res.status(400).json({ error: "Email is required." });
+  }
+  if(!req.body.password){
+    return res.status(400).json({ error: "Password is required." });
+  }
+  if(!req.body.role){
+    return res.status(400).json({ error: "Role is required." });
+  }
+
+  User.find({
+    where: {
+      email: req.body.email,
+    },
+  }).then((user) => {
+    if (user) {
+      return res.status(422).json({ error: "Email already exists." });
+    }
+    else {
+      User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, salt),
+        role: req.body.role,
+      }).then((user) => {
+        res.json({ user: user });
+      }).catch((err) => {
+        res.status(500).json({ error: err });
+      });
+    }
+  });
+});
+
+usersRouter.post('/signin', (req, res) => {
+  User.find({
+    where: {
+      email: req.body.email,
+    },
+  }).then((user) => {
+    if (user === null) {
+      return res.status(401).json({ error: "Incorrect username or password." });
+    }
+    const hash = user.password; // Load hash from your password DB.
+    const password = req.body.password; // this is the password passed in by the user
+    const result = bcrypt.compareSync(password, hash);
+    // password incorrect
+    if (!result) {
+      return res.status(401).json({ error: "Incorrect username or password." });
+    }
+
+    // req.session.userId = user.id;
+    // return res.json({ userInfo: user, userId: req.session.userId });
+  }).catch((err) => {
+    res.status(500).json({ error: err });
+  });
+});
+
+usersRouter.get('/signout', function (req, res, next) {
+  // req.session.destroy();
+  return res.redirect("/");
+});
+
+usersRouter.post('/auth/signup', (req, res) => {
+  const auth = new auth0.WebAuth({
+    domain: domain,
+    clientID: clientID,
+    redirectUri: callbackUrl,
+    logoutUrl: url,
+  });
+
+  auth.signup({
+    connection: 'Username-Password-Authentication',
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username,
+    role: req.body.role,
+  }, (err) => {
+    if (err) {
+      console.log(err);
+      res.redirect('/');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+
+//auth0
+usersRouter.get('/callback', (req, res) => {
+  const auth = new auth0.WebAuth({
+    domain: domain,
+    clientID: clientID,
+    redirectUri: callbackUrl,
+    logoutUrl: url,
+  });
+
+  auth.parseHash((err, authResult) => {
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      // Save tokens to local storage
+      localStorage.setItem('access_token', authResult.accessToken);
+      localStorage.setItem('id_token', authResult.idToken);
+      localStorage.setItem('expires_at', JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime()));
+
+      // Redirect to home page
+      res.redirect('/');
+    } else if (err) {
+      // Handle error
+      console.log(err);
+      res.redirect('/');
+    }
+  });
+});
+
