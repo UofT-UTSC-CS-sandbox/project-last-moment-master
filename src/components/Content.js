f7cb5d2861557e03ccbfe370cbde661511ed7333
Oliiiv <weng.zhang0708@gmail.com>
2023-03-18 16:17:31 -0400

    feat: frontend improvement for navbar and homepage

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import bgImage from "../assets/code_background.jpeg";
 
 class Content extends Component {
+  componentDidMount() {
+    // Set the background image on the body element
+    document.body.style.backgroundImage = `url(${bgImage})`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center center';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.minHeight = '100vh';
+  }
   render() {
     return (
-      <div className="mt-24 next-steps my-5">
-        <h2 className="my-5 text-center">What can I do next?</h2>
+      <div>
+        <h1 className="my-5 text-center mt-80 text-6xl text-[#f2f6ef] font-bold font-mono">
+          Welcome to Skillvitrine
+        </h1>
+        <h2 className="my-5 text-center text-3xl text-[#f2f6ef] font-bold font-mono">
+          A place to showcase your skills
+        </h2>
       </div>
     );
   }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+  <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-[#668f53] border-t border-[#668f53] shadow md:flex md:items-center md:justify-between md:p-6">
+    <span className="text-sm text-[#c8dabc] sm:text-center">
       © 2023{" "}
       <a
         href="https://github.com/orgs/UofT-UTSC-CS-sandbox/teams/last-moment-master"
@@ -12,7 +12,7 @@ const Footer = () => (
       </a>
       . All Rights Reserved.
     </span>
-    <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm text-[#c8dabc] sm:mt-0">
       <li>
         <a href="/" className="mr-4 hover:underline md:mr-6 ">
           About
