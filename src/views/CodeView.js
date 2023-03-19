1624c0e3eb04a8511044cd11335bc86f46ce04c9
王仔很忙 <wanglianting@vip.qq.com>
2023-03-19 15:29:11 -0400

    Feature initialize (#4)
    * feat: initialize frontend and backend
    
    * fix: optimized code and removed unnecessary files
    
    * fix: reduced warnings in code

---
+++ src/views/CodeView.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import { Alert } from "reactstrap";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+import { getConfig } from "../config";
+import Loading from "../components/Loading";
+import CodeArea from "../components/CodeArea";
+
+export const ExternalApiComponent = () => {
+  const { apiOrigin = "http://localhost:3001", audience } = getConfig();
+
+  const [state, setState] = useState({
+    showResult: false,
+    apiMessage: "",
+    error: null,
+  });
+
+  const { getAccessTokenSilently, loginWithPopup, getAccessTokenWithPopup } =
+    useAuth0();
+
+  const handleConsent = async () => {
+    try {
+      await getAccessTokenWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const handleLoginAgain = async () => {
+    try {
+      await loginWithPopup();
+      setState({
+        ...state,
+        error: null,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+
+    await callApi();
+  };
+
+  const callApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(`${apiOrigin}/api/external`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const responseData = await response.json();
+
+      setState({
+        ...state,
+        showResult: true,
+        apiMessage: responseData,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        error: error.error,
+      });
+    }
+  };
+
+  const handle = (e, fn) => {
+    e.preventDefault();
+    fn();
+  };
+
+  return (
+    <div className="mt-24">
+      <div className="mb-5">
+        {state.error === "consent_required" && (
+          <Alert color="warning">
+            You need to{" "}
+            <a
+              href="#/"
+              className="alert-link"
+              onClick={(e) => handle(e, handleConsent)}
+            >
+              consent to get access to users api
+            </a>
+          </Alert>
+        )}
+
+        {state.error === "login_required" && (
+          <Alert color="warning">
+            You need to{" "}
+            <a
+              href="#/"
+              className="alert-link"
+              onClick={(e) => handle(e, handleLoginAgain)}
+            >
+              log in again
+            </a>
+          </Alert>
+        )}
+      </div>
+
+      <div className="mt-24">
+        <h1 className="text-center" id="title">
+          Here is the question
+        </h1>
+      </div>
+
+      <CodeArea />
+
+      <div className="result-block-container">
+        {state.showResult && <div></div>}
+      </div>
+    </div>
+  );
+};
+
+export default withAuthenticationRequired(ExternalApiComponent, {
+  onRedirecting: () => <Loading />,
+});
