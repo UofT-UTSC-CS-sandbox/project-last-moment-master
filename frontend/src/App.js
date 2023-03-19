5c97e7de64a853662663afc3123953eacf857aa2
王仔很忙 <wanglianting@vip.qq.com>
2023-03-19 15:26:50 -0400

    revert: back to the initialize version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
-import Login from "./components/pages/Login/Login.jsx";
-import SignUp from "./components/pages/SignUp/SignUp.jsx";
-import MainPage from "./components/pages/MainPage/MainPage.jsx";
-import { Route, Routes } from "react-router-dom";
-import React from "react";
-import Navbar from "./components/NavBar/Navbar.jsx";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      {/* <h1 className="text-5xl text-green-400">Skill Vitrine</h1> */}
-      <Navbar />
-      <Routes>
-        {/* <Route path='/' exact component={Login}></Route> */}
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/mainpage" element={<MainPage />} />
-      </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
