5c51a319f47770ac63bdc02779241854a2367cea
王仔很忙 <wanglianting@vip.qq.com>
2023-03-30 19:13:32 -0400

    feat: finished judge0 part (#9)
    * feat: finished judge0 part
    
    * fix: fixed code style issues

--- api-server.js
+++ api-server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const bodyParser = require("body-parser");
 const { expressjwt: jwt } = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 const authConfig = require("./src/auth_config.json");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const axios = require("axios");
 
 const app = express();
 
+dotenv.config();
 const port = process.env.API_PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
-dotenv.config();
+const XRapidAPIKey = process.env.XRAPIKEY;
+const XRapidAPIHost = process.env.XRAPIHOST;
 
 try {
   mongoose.connect(process.env.MONGODB_CONNECTION, {
@@ -37,6 +41,7 @@ if (
   process.exit();
 }
 
+app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({ origin: appOrigin }));
@@ -58,6 +63,57 @@ app.get("/api/external", checkJwt, (req, res) => {
   res.send({
     msg: "Your access token was successfully validated!",
   });
+});
+
+app.get("/api/codeViewId", checkJwt, (req, res) => {
+  res.send({
+    id: "examples",
+  });
+});
+
+app.post("/api/execute", checkJwt, (req, res) => {
+  const value = Buffer.from(req.body.code, "utf-8");
+  const valueB64 = value.toString("base64");
+  const inputOptions = {
+    method: "POST",
+    url: "https://judge0-ce.p.rapidapi.com/submissions/",
+    params: { base64_encoded: "true", fields: "*", wait: "true" },
+    headers: {
+      "content-type": "application/json",
+      "Content-Type": "application/json",
+      "X-RapidAPI-Key": XRapidAPIKey,
+      "X-RapidAPI-Host": XRapidAPIHost,
+    },
+    data: `{"language_id":63,"source_code":"${valueB64}","stdin":""}`,
+  };
+  axios
+    .request(inputOptions)
+    .then(function (response) {
+      const results = response.data;
+      console.log(results.token);
+      const outputOptions = {
+        method: "GET",
+        url: `https://judge0-ce.p.rapidapi.com/submissions/${results.token}`,
+        params: { base64_encoded: "true", fields: "*" },
+        headers: {
+          "X-RapidAPI-Key": XRapidAPIKey,
+          "X-RapidAPI-Host": XRapidAPIHost,
+        },
+      };
+      axios
+        .request(outputOptions)
+        .then(function (response) {
+          const resultsB64 = Buffer.from(response.data.stdout, "base64");
+          const results = resultsB64.toString("utf-8");
+          res.send({ results });
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 app.listen(port, () => console.log(`API Server listening on port ${port}`));
