6e9aee2257d9d237826f8ae4bc16d7b4fd758ccb
Oliiiv <weng.zhang0708@gmail.com>
2023-04-05 19:09:20 -0400

    remove unwant files

--- models/questionModel.js
+++
@@ -1,9 +0,0 @@
-const { mongoose, Schema } = require("mongoose");
-
-const questionSchema = new Schema({
-  question: { type: String, required: true },
-  createAt: { type: Date, default: Date.now },
-  updateAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Question", questionSchema);
